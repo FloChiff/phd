b561fd7aa94021769938a602aff25fcde15b04f1
FloChiff <floriane.chiffoleau@hotmail.fr>
2022-04-21 12:39:42 +0200

    Added PAGE XML option to the script

--- word_frequency/scripts/groundtruth.py
+++ word_frequency/scripts/groundtruth.py
@@ -9,8 +9,9 @@
 - output: TXT file
 - usage :
     ======
-    python name_of_this_script.py arg1
+    python name_of_this_script.py arg1 arg2
     arg1: folder of the groundtruth in their XML format
+    arg2: name of the output file in a TXT format
 """
 
 import os
@@ -23,12 +24,17 @@ for root, dirs, files in os.walk(sys.argv[1]):
 
             processed_text_as_list = []
             soup = BeautifulSoup(xml_file, 'xml')
+            #Put one inside air quotes according to the type of files that are worked on
             #Extract content from the XML ALTO files
             for string in soup.find_all("String"):
                 content = string["CONTENT"]
                 processed_text_as_list.append(content)
+            #Extract content from the PAGE XML files
+            for string in soup.find_all("TextEquiv"):
+                content = string.find("Unicode").string
+                processed_text_as_list.append(content)
             new_text = "\n".join(processed_text_as_list)
 
-        with open("ground_truth.txt", "a") as file_out:
+        with open(sys.argv[2], "a") as file_out:
             #Warning: the output file is in a "add" mode, which mean that it will add groundtruth, file after file
             file_out.write(new_text)
